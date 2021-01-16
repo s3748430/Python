34a637ef6a359fbab354ee3b0566aaff34349d8e
s3748430 <62051704+s3748430@users.noreply.github.com>
2021-01-16 13:59:03 +0700

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,19 @@
+num_list = [1,3,5,7,9]
+
+total = 0
+
+for num in num_list:
+    total += num
+print(total)
+
+total = 0
+
+for num in range (0,5):
+    total += num
+print(total)
+
+total = 0
+
+for num in range (5):
+    total += num
+print(total)
